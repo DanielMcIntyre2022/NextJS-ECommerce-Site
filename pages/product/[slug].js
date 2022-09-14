34f29c1fe377f35168a3a03aa29473a829b1750d
Daniel <daniel-mcintyre@hotmail.com>
2022-09-14 13:51:52 -0400

    created product details screen

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,8 @@
 import Layout from '../../components/Layout';
 import {useRouter} from 'next/router';
 import data from '../../utils/data';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const ProductScreen = () => {
 
@@ -12,7 +14,52 @@ const ProductScreen = () => {
     }
     return (
         <Layout title={product.name}>
-            <h1>{product.name}</h1>
+            <div className='py-2'>
+                <Link href="/">Back to products</Link>
+            </div>
+            <div className='grid md:grid-cols-4 md:gap-3'>
+                <div className='md:col-span-2'>
+                    <Image
+                        src={product.image}
+                        alt={product.name}
+                        width={640}
+                        height={640}
+                        layout="responsive"
+                    ></Image>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <h1 className='text-lg'>{product.name}</h1>
+                        </li>
+                        <li>
+                            Category: {product.catergory}
+                        </li>
+                        <li>
+                            Brand: {product.brand}
+                        </li>
+                        <li>
+                            {product.rating} of {product.numReviews} reviews
+                        </li>
+                        <li>
+                            Description: {product.description}
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <div className='card p-5'>
+                    <div className='mb-2 flex justify-between'>
+                        <div>Price</div>
+                        <div>${product.price}</div>
+                    </div>
+                    <div className='mb-2 flex justify-between'>
+                        <div>Status</div>
+                        <div>{product.countInStock > 0 ? 'In Stock' : 'Unavaible'}</div>
+                    </div>
+                    <button className='primary-button w-full'>Add to Cart</button>
+                    </div>
+                </div>
+            </div>
         </Layout>
     )
 }
