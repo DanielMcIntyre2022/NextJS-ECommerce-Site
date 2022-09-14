8d100c238a1260af1a1d5481d9d1dcdd73193b8d
Daniel <daniel-mcintyre@hotmail.com>
2022-09-14 15:42:41 -0400

    displayed cart items on page

--- pages/cart.js
+++ pages/cart.js
@@ -1,7 +1,9 @@
 import { useContext } from "react";
 import { Store } from '../utils/Store';
+import { XCircleIcon } from '@heroicons/react/outline';
 import Layout from '../components/Layout';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const CartScreen = () => {
 
@@ -19,9 +21,53 @@ const CartScreen = () => {
                 </div>
                 ) : (
                     <div className="grid md:grid-cols-4 md:gap-5">
+
+                    <div className="overflow-x-auto md:col-span-3">
+                            <table className="min-w-full">
+                                <thead className="border-b">
+                                    <tr>
+                                        <th className="px-5 text-left">Item</th>
+                                        <th className="p-5 text-right">Quanity</th>
+                                        <th className="p-5 text-right">Price</th>
+                                        <th className="p-5">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {cartItems.map((item) => (
+                                        <tr key={item.slug} className="border-b">
+                                            <td>
+                                                <Link href={`/product/${item.slug}`}>
+                                                    <a className="flex items-center">
+                                                        <Image
+                                                            src={item.image}
+                                                            alt={item.name}
+                                                            width={50}
+                                                            height={50}
+                                                        >
+                                                        </Image>
+                                                        &nbsp;
+                                                        {item.name}
+                                                    </a>
+                                                </Link>
+                                            </td>
+                                            <td className="p-5 text-right">
+                                                {item.quanity}
+                                            </td>
+                                            <td className="p-5 text-right">
+                                                {item.price}
+                                            </td>
+                                            <td className="p-5 text-right">
+                                                <button>
+                                                    <XCircleIcon className="h-5 w-5"></XCircleIcon>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 )
-
             }
         </Layout>
     )
