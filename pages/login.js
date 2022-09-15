894029b9ba7350810f92ece5e9b9536371dee7de
Daniel <daniel-mcintyre@hotmail.com>
2022-09-15 11:42:50 -0400

    created form validation for email address on login page

--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,34 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
+import { useForm } from 'react-hook-form';
 
 const LoginScreen = () => {
+
+    const {
+        handleSubmit,
+        register,
+        formState: { errors },
+    } = useForm();
+
+    const submitHandler = ({ email, password }) => {
+
+    }
+
     return (
         <Layout title="Login">
-            <form className="mx-auto max-w-screen-md">
+            <form className="mx-auto max-w-screen-md" onSubmit={handleSubmit(submitHandler)}>
                 <h1 className="mb-4 text-xl">Login</h1>
                 <div className="mb-4">
-                    <label htmlFor="email">Email</label>
+                    <label htmlFor="email"
+                    {...register('email', {required:'Please enter email',
+                    pattern: {
+                        value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                        message: 'Please enter valid email address'
+                    }
+                })}
+                    >Email</label>
                     <input type="email" className="w-full" id="email" autoFocus></input>
+                    {errors.email && (<div className="text-red-500">{errors.email.message}</div>)}
                 </div>
                 <div className="mb-4">
                     <label htmlFor="password">Password</label>
